509cb7a65957dddf9b3a52ccb628e1afa4d8a6d3
karnwine729 <31371047+karnwine729@users.noreply.github.com>
2024-07-17 20:01:33 +0000

    I have no idea what I'm doing

---
+++ src/GameBoard.java
@@ -0,0 +1,57 @@
+public class GameBoard {
+    private char[] positionValues;
+
+    public GameBoard() {
+        this.positionValues = new char[9];
+        for (int i = 0; i < 9; i++) {
+            positionValues[i] = ' ';
+        }
+    }
+
+    public void displayBoard() {
+        System.out.println();
+        displayTopRow(positionValues);
+        System.out.println("-".repeat(11));
+        displayMiddleRow(positionValues);
+        System.out.println("-".repeat(11));
+        displayBottomRow(positionValues);
+        System.out.println();
+    }
+
+    public void displayTopRow(char[] positionValues) {
+        for (int i = 1; i <= 11; i++) {
+            if (isBlank(i))             System.out.print(" ");
+            if ((i == 4) || (i == 8))   System.out.print("|");
+            if (i == 2)                 System.out.print(positionValues[6]);
+            if (i == 6)                 System.out.print(positionValues[7]);
+            if (i == 10)                System.out.print(positionValues[8]);
+            if (i == 11)                System.out.println();
+        }
+    }
+
+    public void displayMiddleRow(char[] positionValues) {
+        for (int i = 1; i <= 11; i++) {
+            if (isBlank(i))             System.out.print(" ");
+            if ((i == 4) || (i == 8))   System.out.print("|");
+            if (i == 2)                 System.out.print(positionValues[3]);
+            if (i == 6)                 System.out.print(positionValues[4]);
+            if (i == 10)                System.out.print(positionValues[5]);
+            if (i == 11)                System.out.println();
+        }
+    }
+
+    public void displayBottomRow(char[] positionValues) {
+        for (int i = 1; i <= 11; i++) {
+            if (isBlank(i))             System.out.print(" ");
+            if ((i == 4) || (i == 8))   System.out.print("|");
+            if (i == 2)                 System.out.print(positionValues[0]);
+            if (i == 6)                 System.out.print(positionValues[1]);
+            if (i == 10)                System.out.print(positionValues[2]);
+            if (i == 11)                System.out.println();
+        }
+    }
+
+    public boolean isBlank(int n) {
+        return (n == 1) || (n == 3) || (n == 5) || (n == 7) || (n == 9) || (n == 11);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        GameBoard gb = new GameBoard();
+        gb.displayBoard();
+    }
+}
